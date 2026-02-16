c0c736ac50898dc74381fd5c61e50f95530bdc4f
Houssam Ouhra <houssamblur04@gmail.com>
2026-02-16 11:50:26 +0000

    scripts: format time.sh

--- scripts/.config/scripts/time.sh
+++ scripts/.config/scripts/time.sh
@@ -3,15 +3,15 @@
 CATEGORIES=(
     "WORK"
     "NVIM"
-	"WASTE" 
-	"STOP"
+    "WASTE"
+    "STOP"
 )
 
 BLOCKED_DOMAINS=(
-  "www.youtube.com"
-  "www.reddit.com"
-  "www.x.com"
-  "www.linkedin.com"
+    "www.youtube.com"
+    "www.reddit.com"
+    "www.x.com"
+    "www.linkedin.com"
 )
 
 selected=$(printf "%s\n" "${CATEGORIES[@]}" | sk --margin 10% --color="bw" --bind 'q:abort') || exit 0
@@ -19,58 +19,54 @@ selected=$(printf "%s\n" "${CATEGORIES[@]}" | sk --margin 10% --color="bw" --bin
 # ---------- VALIDATION ----------
 is_valid_category=false
 for c in "${CATEGORIES[@]}"; do
-  [[ "$selected" == "$c" ]] && is_valid_category=true
+    [[ "$selected" == "$c" ]] && is_valid_category=true
 done
 
 [[ "$is_valid_category" == false ]] && exit 1
 
 # Functions
-
 update_tmux_status() {
-  local category="$1"
-  local total=""
+    local category="$1"
+    local total=""
 
-  if [[ "$category" != "STOP" ]]; then
-    total=$(timew | awk '/^ *Total/ {print $NF}')
-  fi
+    if [[ "$category" != "STOP" ]]; then
+        total=$(timew | awk '/^ *Total/ {print $NF}')
+    fi
 
-  tmux set -g @time_category "$category"
-  tmux set -g @time_total "$total"
+    tmux set -g @time_category "$category"
+    tmux set -g @time_total "$total"
 }
 
 block_sites() {
-  sudo -v
-  for domain in "${BLOCKED_DOMAINS[@]}"; do
-    sudo hostess add "$domain" 127.0.0.1 >/dev/null 2>&1 || true
-  done
+    sudo -v
+    for domain in "${BLOCKED_DOMAINS[@]}"; do
+        sudo hostess add "$domain" 127.0.0.1 >/dev/null 2>&1 || true
+    done
 }
 
 unblock_sites() {
-  sudo -v
-  for domain in "${BLOCKED_DOMAINS[@]}"; do
-    sudo hostess rm "$domain" >/dev/null 2>&1 || true
-  done
+    sudo -v
+    for domain in "${BLOCKED_DOMAINS[@]}"; do
+        sudo hostess rm "$domain" >/dev/null 2>&1 || true
+    done
 }
 
 # ---------- CATEGORY DISPATCH ----------
 case "$selected" in
-  STOP)
-    timew stop >/dev/null 2>&1 
+STOP)
+    timew stop >/dev/null 2>&1
     tmux set -g @time_category ""
-    paplay ~/.config/waybar/sounds/timer.mp3 &
     unblock_sites
     exit 0
     ;;
-  WASTE)
+WASTE)
     timew start "$selected" >/dev/null 2>&1
-    tmux set -g @time_category "WASTE" 
-    paplay ~/.config/waybar/sounds/timer.mp3 &
-	unblock_sites
-	;;
-  WORK|NVIM)
+    tmux set -g @time_category "WASTE"
+    unblock_sites
+    ;;
+WORK | NVIM)
     timew start "$selected" >/dev/null 2>&1
     tmux set -g @time_category "WORK"
-    paplay ~/.config/waybar/sounds/timer.mp3 &
     block_sites
     ;;
 esac
