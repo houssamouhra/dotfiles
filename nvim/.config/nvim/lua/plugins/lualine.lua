82a9d21dd4d2676845f7970ff8edb94f2e8056db
Houssam Ouhra <houssamblur04@gmail.com>
2026-02-18 19:00:49 +0000

    nvim: refactor lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -7,29 +7,8 @@ return {
   },
 
   config = function()
-    local filename = {
-      'filename',
-      file_status = true, -- displays file status (readonly status, modified status)
-      path = 0,
-    }
-
     local hide_in_width = function()
       return vim.fn.winwidth(0) > 100
-    end
-
-    local ok, pomo = pcall(require, 'pomo')
-    local pomo_timer = function()
-      return ''
-    end
-
-    if ok then
-      pomo_timer = function()
-        local timer = pomo.get_first_to_finish()
-        if timer then
-          return '󰄉 ' .. tostring(timer)
-        end
-        return ''
-      end
     end
 
     local diagnostics = {
@@ -46,7 +25,7 @@ return {
     local diff = {
       'diff',
       colored = true,
-      symbols = { added = '+', modified = '~', removed = '-' }, -- changes diff symbols
+      symbols = { added = '+', modified = '~', removed = '-' },
       cond = hide_in_width,
     }
 
@@ -58,12 +37,12 @@ return {
         component_separators = { '', '' },
         globalstatus = true,
       },
-      refresh = { -- sets how often lualine should refresh it's contents (in ms)
+      refresh = {
         statusline = 100,
         tabline = 100,
         winbar = 100,
-        refresh_time = 16, -- ~60fps the time after which refresh queue is processed. Mininum refreshtime for lualine
-        events = { -- The auto command events at which lualine refreshes
+        refresh_time = 16,
+        events = {
           'WinEnter',
           'BufEnter',
           'BufWritePost',
@@ -80,16 +59,18 @@ return {
       sections = {
         lualine_a = { 'mode' },
         lualine_b = { 'branch', diff },
-        lualine_c = { filename },
-
+        lualine_c = {
+          {
+            'filename',
+            file_status = true,
+          },
+        },
         lualine_x = {
-          pomo_timer,
           diagnostics,
           'encoding',
           'fileformat',
           'filetype',
         },
-
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
       },
