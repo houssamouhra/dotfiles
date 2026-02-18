9f2840f3664edf69009f1b88ac48e80e7e67bead
Houssam Ouhra <houssamblur04@gmail.com>
2026-02-18 19:39:47 +0000

    nvim: refactor tokyonight theme

--- nvim/.config/nvim/lua/themes/tokyonight/init.lua
+++ nvim/.config/nvim/lua/themes/tokyonight/init.lua
@@ -1,30 +1,33 @@
 return {
   'folke/tokyonight.nvim',
+  lazy = true,
+  event = 'UIEnter',
   config = function()
     require('tokyonight').setup {
-      lazy = false,
       priority = 1000,
-      opts = {},
-      style = 'night', -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-      light_style = 'day', -- The theme is used when the background is set to light
-      transparent = true, -- Enable this to disable setting the background color
-      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      style = 'night',
+      light_style = 'day',
+      transparent = true,
+      terminal_colors = true,
       styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
+        comments = { fg = '#565f89', italic = true },
+        NormalFloat = { bg = '#1a1b26' },
         keywords = { italic = true },
         functions = {},
         variables = {},
-        -- Background styles. Can be "dark", "transparent" or "normal"
         sidebars = 'transparent',
-        floats = 'transparent', -- style for floating windows
+        floats = 'transparent',
       },
-      day_brightness = 0.3, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-      dim_inactive = false, -- dims inactive windows
-      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+      day_brightness = 0.3,
+      dim_inactive = false,
+      lualine_bold = false,
     }
 
     vim.cmd.colorscheme 'tokyonight-night'
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = function()
+        require('transparent').clear_prefix 'Lualine'
+      end,
+    })
   end,
 }
