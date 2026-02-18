553106520dadaa257d10ea713a24182c01d003c3
Houssam Ouhra <houssamblur04@gmail.com>
2026-02-18 19:39:25 +0000

    nvim: refactor night-owl theme

--- nvim/.config/nvim/lua/themes/night-owl/init.lua
+++ nvim/.config/nvim/lua/themes/night-owl/init.lua
@@ -6,9 +6,8 @@ return {
       'oxfist/night-owl.nvim',
       lazy = false, -- make sure we load this during startup if it is your main colorscheme
       priority = 1000, -- make sure to load this before all the other start plugins
-      config = function()
-        -- load the colorscheme here
-      end,
+      transparent_background = true,
+      config = function() end,
     }
     vim.cmd.colorscheme 'night-owl'
   end,
