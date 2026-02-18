6981e7ca6f131b97b28a92d13e23bfc645f81769
Houssam Ouhra <houssamblur04@gmail.com>
2026-02-18 18:55:46 +0000

    nvim: refactor cmdline plugin

--- nvim/.config/nvim/lua/plugins/cmdline.lua
+++ nvim/.config/nvim/lua/plugins/cmdline.lua
@@ -3,23 +3,152 @@ return {
   event = 'VeryLazy',
   dependencies = {
     'MunifTanjim/nui.nvim',
-    {
-      'rcarriga/nvim-notify',
-      config = function()
-        require('notify').setup {
-          background_colour = '#1e1e2e',
-        }
-      end,
-    },
+    'rcarriga/nvim-notify',
   },
   config = function()
     require('noice').setup {
+      cmdline = {
+        enabled = true,
+        view = 'cmdline_popup',
+        opts = {},
+        format = {
+          cmdline = { pattern = '^:', icon = ' ', lang = 'vim' },
+          search_down = { kind = 'search', pattern = '^/', icon = ' ▼', lang = 'regex' },
+          search_up = { kind = 'search', pattern = '^%?', icon = ' ▲', lang = 'regex' },
+          filter = { pattern = '^:%s*!', icon = '$', lang = 'bash' },
+          lua = { pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' }, icon = '', lang = 'lua' },
+          help = { pattern = '^:%s*he?l?p?%s+', icon = '' },
+        },
+      },
+      messages = {
+        enabled = true,
+        view = 'notify',
+        view_error = 'notify',
+        view_warn = 'notify',
+        view_history = 'messages',
+        view_search = 'virtualtext',
+      },
+      popupmenu = {
+        enabled = true,
+        backend = 'nui',
+      },
+      redirect = {
+        view = 'popup',
+        filter = { event = 'msg_show' },
+      },
+      ---@type table<string, NoiceCommand>
+      commands = {
+        history = {
+          -- options for the message history that you get with `:Noice`
+          view = 'split',
+          opts = { enter = true, format = 'details' },
+          filter = {
+            any = {
+              { event = 'notify' },
+              { error = true },
+              { warning = true },
+              { event = 'msg_show', kind = { '' } },
+              { event = 'lsp', kind = 'message' },
+            },
+          },
+        },
+        -- :Noice last
+        last = {
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = {
+            any = {
+              { event = 'notify' },
+              { error = true },
+              { warning = true },
+              { event = 'msg_show', kind = { '' } },
+              { event = 'lsp', kind = 'message' },
+            },
+          },
+          filter_opts = { count = 1 },
+        },
+        -- :Noice errors
+        errors = {
+          view = 'popup',
+          opts = { enter = true, format = 'details' },
+          filter = { error = true },
+          filter_opts = { reverse = true },
+        },
+        all = {
+          view = 'split',
+          opts = { enter = true, format = 'details' },
+          filter = {},
+        },
+      },
+      notify = {
+        enabled = true,
+        view = 'notify',
+      },
       lsp = {
+        progress = {
+          enabled = true,
+          format = 'lsp_progress',
+          format_done = 'lsp_progress_done',
+          throttle = 1000 / 30, -- frequency to update lsp progress message
+          view = 'mini',
+        },
         override = {
           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
           ['vim.lsp.util.stylize_markdown'] = true,
           ['cmp.entry.get_documentation'] = true,
         },
+        hover = {
+          enabled = true,
+          silent = false,
+          view = nil,
+          ---@type NoiceViewOptions
+          opts = {},
+        },
+        signature = {
+          enabled = true,
+          auto_open = {
+            enabled = true,
+            trigger = true,
+            luasnip = true,
+            throttle = 50,
+          },
+          view = nil,
+          opts = {},
+        },
+        message = {
+          -- Messages shown by lsp servers
+          enabled = true,
+          view = 'notify',
+          opts = {},
+        },
+        -- defaults for hover and signature help
+        documentation = {
+          view = 'hover',
+          opts = {
+            lang = 'markdown',
+            replace = true,
+            render = 'plain',
+            format = { '{message}' },
+            win_options = { concealcursor = 'n', conceallevel = 3 },
+          },
+        },
+      },
+      markdown = {
+        hover = {
+          ['|(%S-)|'] = vim.cmd.help,
+          ['%[.-%]%((%S-)%)'] = require('noice.util').open,
+        },
+        highlights = {
+          ['|%S-|'] = '@text.reference',
+          ['@%S+'] = '@parameter',
+          ['^%s*(Parameters:)'] = '@text.title',
+          ['^%s*(Return:)'] = '@text.title',
+          ['^%s*(See also:)'] = '@text.title',
+          ['{%S-}'] = '@parameter',
+        },
+      },
+      health = {
+        checker = true,
       },
       presets = {
         bottom_search = false,
@@ -28,6 +157,7 @@ return {
         inc_rename = false,
         lsp_doc_border = false,
       },
+      throttle = 1000 / 30,
     }
   end,
 }
