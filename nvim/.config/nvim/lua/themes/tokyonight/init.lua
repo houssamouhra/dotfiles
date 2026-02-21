b86f9513b0fedd45fe618b635db10f835f61bff3
Houssam Ouhra <houssamblur04@gmail.com>
2026-02-21 01:56:09 +0000

    nvim(tokyonight): lazy-load on VeryLazy and refine transparency

--- nvim/.config/nvim/lua/themes/tokyonight/init.lua
+++ nvim/.config/nvim/lua/themes/tokyonight/init.lua
@@ -1,7 +1,7 @@
 return {
   'folke/tokyonight.nvim',
   lazy = true,
-  event = 'UIEnter',
+  event = 'VeryLazy',
   config = function()
     require('tokyonight').setup {
       priority = 1000,
@@ -13,21 +13,21 @@ return {
         comments = { fg = '#565f89', italic = true },
         NormalFloat = { bg = '#1a1b26' },
         keywords = { italic = true },
-        functions = {},
-        variables = {},
         sidebars = 'transparent',
         floats = 'transparent',
       },
       day_brightness = 0.3,
       dim_inactive = false,
       lualine_bold = false,
+
+      on_colors = function(colors)
+        colors.bg_statusline = colors.none -- forces statusline / tabline fill to transparent
+        -- Optional extras if you still see issues:
+        -- colors.bg_dark     = colors.none
+        -- colors.bg_float    = colors.none
+      end,
     }
 
     vim.cmd.colorscheme 'tokyonight-night'
-    vim.api.nvim_create_autocmd('ColorScheme', {
-      callback = function()
-        require('transparent').clear_prefix 'Lualine'
-      end,
-    })
   end,
 }
