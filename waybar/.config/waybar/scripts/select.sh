2a5826fb49d9111f61481388efcf3b2e9bf858e8
Houssam Ouhra <houssamblur04@gmail.com>
2026-02-27 23:30:27 +0000

    waybar: refactor select.sh

--- waybar/.config/waybar/scripts/select.sh
+++ waybar/.config/waybar/scripts/select.sh
@@ -5,37 +5,36 @@ STYLECSS="$WAYBAR_DIR/style.css"
 THEMES="$WAYBAR_DIR/themes"
 
 menu() {
-    printf "clean"
+	printf "clean"
 }
 
 apply_theme() {
-    case "$1" in
-        clean)
-            cp "$THEMES/clean.css" "$STYLECSS"
-            ;;
-        *)
-            exit 0
-            ;;
-    esac
+	case "$1" in
+	clean)
+		cp "$THEMES/clean.css" "$STYLECSS"
+		;;
+	*)
+		exit 0
+		;;
+	esac
 }
 
 reload_waybar() {
-    pkill waybar
-    sleep 0.15
-    waybar &
+	pkill waybar
+	sleep 0.15
+	waybar &
 }
 
 main() {
-    choice=$(menu | wofi \
-        --show dmenu \
-        --prompt " Waybar Theme " \
-        -n)
+	choice=$(menu | wofi \
+		--show dmenu \
+		--prompt " Waybar Theme " \
+		-n)
 
-    [ -z "$choice" ] && exit 0
+	[ -z "$choice" ] && exit 0
 
-    apply_theme "$choice"
-    reload_waybar
+	apply_theme "$choice"
+	reload_waybar
 }
 
 main
-
