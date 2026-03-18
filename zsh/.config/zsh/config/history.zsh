3870665c872fe84762303c0d0e3e592e67f3f7f5
Houssam Ouhra <houssamblur04@gmail.com>
2026-03-18 02:20:59 +0000

    zsh: update history config

--- zsh/.config/zsh/config/history.zsh
+++ zsh/.config/zsh/config/history.zsh
@@ -1,5 +1,11 @@
 HISTSIZE=100000
 HISTFILE="$XDG_CACHE_HOME/zsh_history"
 SAVEHIST=$HISTSIZE
-setopt sharehistory hist_ignore_all_dups appendhistory
-setopt hist_reduce_blanks hist_verify inc_append_history
+setopt sharehistory
+setopt hist_ignore_all_dups
+setopt appendhistory
+setopt hist_reduce_blanks
+setopt hist_verify
+setopt hist_expire_dups_first
+setopt inc_append_history
+setopt hist_find_no_dups
