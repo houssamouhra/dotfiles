af33781ac4bb3096d3e180f3a9d0d30d6c0a9f5e
Houssam Ouhra <houssamblur04@gmail.com>
2026-03-22 13:32:07 +0100

    nvim: clean up tokyonight config

--- nvim/.config/nvim/lua/themes/tokyonight/init.lua
+++ nvim/.config/nvim/lua/themes/tokyonight/init.lua
@@ -5,13 +5,11 @@ return {
   event = 'VeryLazy',
   config = function()
     require('tokyonight').setup {
-      priority = 1000,
       style = 'night',
       light_style = 'day',
       transparent = true,
       terminal_colors = true,
       styles = {
-        comments = { fg = '#565f89', italic = true },
         NormalFloat = { bg = '#1a1b26' },
         keywords = { italic = true },
         sidebars = 'transparent',
@@ -20,12 +18,8 @@ return {
       day_brightness = 0.3,
       dim_inactive = false,
       lualine_bold = false,
-
       on_colors = function(colors)
-        colors.bg_statusline = colors.none -- forces statusline / tabline fill to transparent
-        -- Optional extras if you still see issues:
-        -- colors.bg_dark     = colors.none
-        -- colors.bg_float    = colors.none
+        colors.bg_statusline = colors.none
       end,
     }
 
