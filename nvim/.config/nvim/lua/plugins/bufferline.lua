b0447ea8b519267419c5a9a1510d655198ef1a0a
Houssam Ouhra <houssamblur04@gmail.com>
2026-03-23 17:55:34 +0100

    nvim: enable LSP diagnostics and Neo-tree offset

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -26,8 +26,7 @@ return {
         max_name_length = 30,
         max_prefix_length = 30,
         tab_size = 21,
-        diagnostics = false,
-        diagnostics_update_in_insert = false,
+        diagnostics = 'nvim_lsp',
         color_icons = true,
         show_buffer_icons = true,
         show_buffer_close_icons = true,
@@ -37,6 +36,14 @@ return {
         enforce_regular_tabs = true,
         always_show_bufferline = true,
         show_tab_indicators = false,
+        offsets = {
+          {
+            filetype = 'neo-tree',
+            text = 'Neo-tree',
+            highlight = 'Directory',
+            text_align = 'left',
+          },
+        },
         indicator = {
           style = 'none',
         },
