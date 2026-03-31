6e73d971933f4ae9baf094b16ecd5c98d9e8d668
Houssam Ouhra <houssamblur04@gmail.com>
2026-03-31 11:23:28 +0100

    nvim: enhance mini.icons with dev tooling and custom highlights

--- nvim/.config/nvim/lua/plugins/icons.lua
+++ nvim/.config/nvim/lua/plugins/icons.lua
@@ -4,16 +4,35 @@ return {
   lazy = true,
   init = function()
     require('mini.icons').setup {
-      opts = {
-        file = {
-          ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
-          ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
-        },
-        filetype = {
-          dotenv = { glyph = '', hl = 'MiniIconsYellow' },
-        },
+      file = {
+        ['.keep'] = { glyph = '󰊢', hl = 'MiniIconsGrey' },
+        ['devcontainer.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['vite.config.js'] = { glyph = '󱐋', hl = 'MiniIconsVite' },
+        ['vite.config.ts'] = { glyph = '󱐋', hl = 'MiniIconsVite' },
+        ['vite.config.mjs'] = { glyph = '󱐋', hl = 'MiniIconsVite' },
+        ['README.md'] = { glyph = '󰂺', hl = 'MiniIconsGreen' },
+        ['eslint.config.js'] = { glyph = '', hl = 'MiniIconsEslint' },
+        ['tsconfig.app.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['tsconfig.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['tsconfig.node.json'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['commitlint.config.js'] = { glyph = '', hl = 'MiniIconsAzure' },
+        ['.prettierrc'] = { glyph = '', hl = 'MiniIconsYellow' },
+        ['.gitignore'] = { glyph = '', hl = 'MiniIconsGit' },
+        ['.gitattributes'] = { glyph = '', hl = 'MiniIconsGit' },
+      },
+
+      filetype = {
+        dotenv = { glyph = '', hl = 'MiniIconsYellow' },
+        zsh = { glyph = '', hl = 'MiniIconsGreen' },
       },
     }
     require('mini.icons').mock_nvim_web_devicons()
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      callback = function()
+        vim.api.nvim_set_hl(0, 'MiniIconsVite', { fg = '#9752FF' })
+        vim.api.nvim_set_hl(0, 'MiniIconsEslint', { fg = '#8181F2' })
+        vim.api.nvim_set_hl(0, 'MiniIconsGit', { fg = '#F05133' })
+      end,
+    })
   end,
 }
