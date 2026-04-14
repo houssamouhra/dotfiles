07a0a77f5d427799522b36e6e0568c930b66aa3e
Houssam Ouhra <houssamblur04@gmail.com>
2026-04-14 18:05:02 +0100

    nvim: integrate nvim-ts-context-commentstring with Comment.nvim

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,23 +1,28 @@
 return {
   'numToStr/Comment.nvim',
   event = 'BufReadPost',
+  dependencies = {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+  },
   config = function()
-    require('Comment').setup()
+    require('ts_context_commentstring').setup {
+      enable_autocmd = false,
+    }
+
+    require('Comment').setup {
+      pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+    }
 
     local api = require 'Comment.api'
     local map = vim.keymap.set
     local opts = { noremap = true, silent = true }
 
-    -- NORMAL mode: Toggle comment on current line
     map('n', '<C-/>', api.toggle.linewise.current, opts)
+    map('n', '<C-_>', api.toggle.linewise.current, opts)
 
-    -- VISUAL mode: Toggle comment on selection
     map('v', '<C-/>', function()
       api.toggle.linewise(vim.fn.visualmode())
     end, opts)
-
-    -- fallback for terminals that send <C-_>
-    map('n', '<C-_>', api.toggle.linewise.current, opts)
     map('v', '<C-_>', function()
       api.toggle.linewise(vim.fn.visualmode())
     end, opts)
