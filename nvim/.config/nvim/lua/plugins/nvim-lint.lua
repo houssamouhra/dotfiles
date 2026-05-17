447510063f1c84fee7ed9879c1146a6891d5aafd
Houssam Ouhra <houssamblur04@gmail.com>
2026-05-17 23:52:57 +0100

    nvim: update nvim-lint plugin

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -10,7 +10,6 @@ return {
       javascriptreact = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
       vue = { 'eslint_d' },
-      json = { 'eslint_d' },
       python = { 'ruff', 'mypy' },
       sh = { 'shellcheck' },
       markdown = { 'vale' },
