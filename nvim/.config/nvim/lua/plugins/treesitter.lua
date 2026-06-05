f1adc1aebea1e6f1aec66ec12fc5fb85a5abc2f7
Houssam Ouhra <houssamblur04@gmail.com>
2026-06-06 00:58:52 +0100

    nvim: update treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -29,7 +29,9 @@ return {
       },
       sync_install = false,
       auto_install = true,
-      indent = { enable = true },
+      indent = {
+        enable = true,
+      },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
