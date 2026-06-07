47d7ba161f0252bc1fc7eda32261caa96fdbd8fc
Houssam Ouhra <houssamblur04@gmail.com>
2026-06-07 17:36:36 +0100

    nvim: add custom highlights on tokyonight theme

--- nvim/.config/nvim/lua/themes/tokyonight/init.lua
+++ nvim/.config/nvim/lua/themes/tokyonight/init.lua
@@ -21,6 +21,30 @@ return {
       on_colors = function(colors)
         colors.bg_statusline = colors.none
       end,
+
+      on_highlights = function(hl, c)
+        hl['@tag.tsx'] = { fg = c.red }
+
+        hl['@type'] = { fg = c.blue, bold = true }
+        hl['@type.builtin'] = { fg = c.cyan, bold = true }
+        hl['@type.definition'] = { fg = c.yellow, bold = true }
+
+        hl['@lsp.type.interface'] = { fg = c.teal, bold = true }
+
+        hl['@keyword'] = { fg = c.magenta, italic = true }
+        hl['@keyword.function'] = { fg = c.magenta, italic = true }
+
+        hl['@keyword.import.typescript'] = { fg = c.cyan }
+        hl['@keyword.export.typescript'] = { fg = c.cyan }
+
+        hl['@function'] = { fg = c.blue }
+        hl['@function.method'] = { fg = c.cyan }
+        hl['@function.call'] = { fg = c.cyan }
+
+        hl['@parameter'] = { fg = c.fg, italic = true }
+
+        hl['@lsp.type.property'] = { fg = c.fg_dark, italic = true }
+      end,
     }
 
     vim.cmd.colorscheme 'tokyonight-night'
