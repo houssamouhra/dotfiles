900eb35877b1b20b0e70f6c5f73e54310bf87d02
Houssam Ouhra <houssamblur04@gmail.com>
2026-06-27 15:30:38 +0100

    zsh: update keybinds

--- zsh/.config/zsh/config/keybinds.zsh
+++ zsh/.config/zsh/config/keybinds.zsh
@@ -1,11 +1,25 @@
 bindkey -e
-bindkey "^a" beginning-of-line
-bindkey "^e" end-of-line
+
+# Cursor movement
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
 bindkey '^j' backward-char
 bindkey '^k' forward-char
+
+# Editing
 bindkey '^w' backward-kill-word
 bindkey '^u' backward-kill-line
 bindkey '^_' undo
+
+# History
 bindkey '^[[A' history-beginning-search-backward
 bindkey '^[[B' history-beginning-search-forward
-bindkey -s ^f "tmux-sessionizer"
+bindkey '^P' history-substring-search-up
+bindkey '^N' history-substring-search-down
+
+# Shortcuts
+bindkey -s '^f' 'tmux-sessionizer\n'
+
+# History substring configuration
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bold,underline'
