d9ca85a87936088740117cd1fe25bf713f1b43aa
Houssam Ouhra <houssamblur04@gmail.com>
2026-07-06 13:28:02 +0100

    nvim: simplify plugin loading

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -20,46 +20,8 @@ require 'core.options'
 require 'core.keymaps'
 require 'core.diagnostics'
 
--- Set up plugins
+-- Load plugins & theme
 require('lazy').setup {
-  -- Load theme
-  require 'themes.tokyonight',
-
-  -- Load plugins
-  require 'plugins.dashboard',
-  require 'plugins.autocompletion',
-  require 'plugins.bufferline',
-  require 'plugins.cmdline',
-  require 'plugins.trouble',
-  require 'plugins.comment',
-  require 'plugins.nvim-ts-autotag',
-  require 'plugins.dadbod',
-  require 'plugins.nvim-autopairs',
-  require 'plugins.nvim-colorizer',
-  require 'plugins.nvim-surround',
-  require 'plugins.nvim-lint',
-  require 'plugins.debugging',
-  require 'plugins.diffview',
-  require 'plugins.gitsigns',
-  require 'plugins.icons',
-  require 'plugins.oil',
-  require 'plugins.inline-diagnostic',
-  require 'plugins.indent-blankline',
-  require 'plugins.lsp',
-  require 'plugins.lualine',
-  require 'plugins.markdown',
-  require 'plugins.misc',
-  require 'plugins.undotree',
-  require 'plugins.neo-scroll',
-  require 'plugins.neo-tree',
-  require 'plugins.conform',
-  require 'plugins.plenary',
-  require 'plugins.snacks',
-  require 'plugins.telescope-fzf-native',
-  require 'plugins.telescope-ui-select',
-  require 'plugins.telescope',
-  require 'plugins.tmux-navigator',
-  require 'plugins.treesitter',
-  require 'plugins.vim-test',
-  require 'plugins.window-picker',
+  { import = 'plugins' },
+  { import = 'theme' },
 }
