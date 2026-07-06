94c7674c8678074e7756810ca05068cf14fb8017
Houssam Ouhra <houssamblur04@gmail.com>
2026-07-06 12:48:51 +0100

    nvim: update snacks.lua

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -54,13 +54,6 @@ return {
       desc = 'Select Scratch Buffer',
     },
     {
-      '<leader>`',
-      function()
-        Snacks.terminal()
-      end,
-      desc = 'Toggle Terminal',
-    },
-    {
       '<leader>gB',
       function()
         Snacks.gitbrowse()
@@ -95,24 +88,6 @@ return {
         Snacks.lazygit.log()
       end,
       desc = 'Lazygit Log (cwd)',
-    },
-    {
-      '<leader>N',
-      desc = 'Neovim News',
-      function()
-        Snacks.win {
-          file = vim.api.nvim_get_runtime_file('doc/news.txt', false)[1],
-          width = 0.6,
-          height = 0.8,
-          wo = {
-            spell = false,
-            wrap = false,
-            signcolumn = 'yes',
-            statuscolumn = ' ',
-            conceallevel = 3,
-          },
-        }
-      end,
     },
   },
 }
