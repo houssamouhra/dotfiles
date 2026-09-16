7ed488eb3357cbe8a7108cb98f70970a906445a3
Houssam Ouhra <149302326+houssamouhra@users.noreply.github.com>
2026-09-16 16:45:19 +0100

    update lock.sh

--- hypr/.config/hypr/scripts/lock.sh
+++ hypr/.config/hypr/scripts/lock.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-if hyprctl monitors | grep -q "^Monitor HDMI-A-1 "; then
+if hyprctl monitors | grep -q '^Monitor HDMI-A-1'; then
   exec hyprlock -c ~/.config/hypr/hyprlock/external.conf
 else
   exec hyprlock -c ~/.config/hypr/hyprlock/laptop.conf
