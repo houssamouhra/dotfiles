ca898fd10430463cbca0168766adc6cbbada37eb
Houssam Ouhra <149302326+houssamouhra@users.noreply.github.com>
2026-09-23 18:54:55 +0000

    update waybar

--- waybar/.config/waybar/scripts/mpc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-export MPD_HOST="/run/user/1000/mpd.socket"
-exec mpc "$@"
--- waybar/.config/waybar/scripts/rmpc.sh
+++ waybar/.config/waybar/scripts/rmpc.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Tell mpc to use your socket
-export MPD_HOST="/run/user/1000/mpd.socket"
-
 status=$(mpc status 2>/dev/null | sed -n '2p')
 title=$(mpc current -f "%title%" 2>/dev/null)
 
